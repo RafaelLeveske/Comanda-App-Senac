23d39c9b7d86b57a477f24d67000e74de5621a4a
Christian Toledo <christian.odt@gmail.com>
2020-04-05 18:47:40 -0300

    add current focus on request inputs

--- src/pages/Requests/index.js
+++ src/pages/Requests/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   View,
   KeyboardAvoidingView,
@@ -11,6 +11,12 @@ import { Form, FormInput } from "./stylesComponents";
 
 export default function Requests({ route }) {
   const { table } = route.params;
+
+  const fichaRef = useRef();
+  const cozinhaRef = useRef();
+  const produtoRef = useRef();
+  const obsRef = useRef();
+
   return (
     <KeyboardAvoidingView style={styles.container}>
       <View style={styles.header}>
@@ -25,6 +31,7 @@ export default function Requests({ route }) {
           autoCapitalize="none"
           placeholder="Ficha"
           returnKeyType="next"
+          onSubmitEditing={() => cozinhaRef.current.focus()}
         />
         <FormInput
           label="Nº Cozinha"
@@ -32,6 +39,8 @@ export default function Requests({ route }) {
           autoCapitalize="none"
           placeholder="Nº Cozinha"
           returnKeyType="next"
+          ref={cozinhaRef}
+          onSubmitEditing={() => produtoRef.current.focus()}
         />
         <FormInput
           label="Produto"
@@ -39,6 +48,8 @@ export default function Requests({ route }) {
           autoCapitalize="none"
           placeholder="Produto"
           returnKeyType="next"
+          ref={produtoRef}
+          onSubmitEditing={() => obsRef.current.focus()}
         />
         <FormInput
           label="Observação"
@@ -46,6 +57,8 @@ export default function Requests({ route }) {
           autoCapitalize="none"
           placeholder="Observação"
           returnKeyType="next"
+          ref={obsRef}
+          onSubmitEditing={() => {}}
         />
 
         <TouchableOpacity style={styles.newOrder} onPress={() => {}}>
